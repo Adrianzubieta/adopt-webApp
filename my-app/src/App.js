6c56bb944c33d1c2cbe0adb7bc5f9eb2f32dfb03
AdrianZubietaH <adrianzubietah@gmail.com>
2019-03-20 06:57:45 -0300

    agregue react-jss

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+        <Home />
     );
   }
 }
---
+++ my-app/src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+
+}
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Adopt</h1>
+            </div>
+        );
+    }
+}
+
+export default injectSheet(styles)(Home);
