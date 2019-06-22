d921c93b1e9c0122769ad731dbbc2bc8b93f21f3
AdrianZubietaH <adrianzubietah@gmail.com>
2019-06-22 12:44:47 -0300

    agregue la autenticacion de facebook y google

--- my-app/src/cliente/pages/login/Login.jsx
+++ my-app/src/cliente/pages/login/Login.jsx
@@ -66,7 +66,7 @@ const styles = theme => ({
     }
 })
 
-var provider = new firebase.auth.FacebookAuthProvider();
+
 
 class Login extends React.Component {
     constructor(props) {
@@ -91,6 +91,7 @@ class Login extends React.Component {
     }
 
     onClickFacebook = () => {
+        var provider = new firebase.auth.FacebookAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // This gives you a Facebook Access Token. You can use it to access the Facebook API.
             var token = result.credential.accessToken;
@@ -110,8 +111,23 @@ class Login extends React.Component {
     }
 
     onClickGoogle = () => {
-        console.log('Click')
-
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
     }
 
     render() {
--- my-app/src/firebaseConfig.js
+++ my-app/src/firebaseConfig.js
@@ -1,9 +1,8 @@
-const firebaseConfig = {
+export default {
     apiKey: "AIzaSyADLLUEIiJFykTgn_lCcO2jyOD4xfZ2Z9w",
     authDomain: "adop-9279b.firebaseapp.com",
     databaseURL: "https://adop-9279b.firebaseio.com",
     projectId: "adop-9279b",
     storageBucket: "adop-9279b.appspot.com",
     messagingSenderId: "949352984084",
-    appId: "1:949352984084:web:d925b2854e9a4588"
 };
